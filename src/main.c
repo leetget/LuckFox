e396387af2db6543199408087ee5d8e97b763226
not animal <sozykin25@gmail.com>
2025-07-17 18:51:03 +0500

    modify_init

--- src/main.c
+++ src/main.c
@@ -150,8 +150,10 @@ int main(int argc, char **argv)
 #endif
 
     /*Create a Demo*/
-    lv_demo_widgets();
-    lv_demo_widgets_start_slideshow();
+    //lv_demo_widgets();
+    //lv_demo_widgets_start_slideshow();
+    lv_demo_schedule();
+    lv_demo_schedule_start();
 
     /* Enter the run loop of the selected backend */
     driver_backends_run_loop();
